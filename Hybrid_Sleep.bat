29d042d6da3aa45582e55f04dae615d0f7fc6be8
SanDust-77 <32069685+vungsung@users.noreply.github.com>
2018-04-05 03:57:10 +0800

    Performance Optimistic

--- Hybrid_Sleep.bat
+++ Hybrid_Sleep.bat
@@ -13,12 +13,12 @@ REM Get Hybrid_Sleep State / 取得混合式睡眠當前設定
 set flag=0
 set hybri=0
 for /f "skip=15 delims=" %%a in ('powercfg -Q %active_scheme% 238c9fa8-0aad-41ed-83f4-97be242c8f20') do (
-	if !flag! lss 2 (
-		set process=%%a
-		set /a hybri+=!process:~-1!
-	)
+	set process=%%a
+	set /a hybri+=!process:~-1!
 	set /a flag+=1
+	if "!flag!"=="2" goto stop
 )
+:stop
 if %hybri% lss 2 (
 	set Hybrid_Sleep_Index=0
 	set Hybrid_Sleep=Off
@@ -30,15 +30,15 @@ if %hybri% lss 2 (
 REM Get Hibernate State / 取得休眠當前設定
 if exist "%SystemDrive%\hiberfil.sys" (
 	set Hibernate=On
-	set flag=0
 	for /f "skip=4 tokens=4 delims= " %%a in ('dir /A %SystemDrive%\hiberfil.sys') do (
-		if "!flag!"=="0" (set "file_size= (%%a Bytes)")
-		set /a flag+=1
+		set "file_size= (%%a Bytes)"
+		goto break
 	)
 ) else (
 	set Hibernate=Off
 	set file_size=
 )
+:break
 
 REM Give Choices & Take Actions / 提供動作選項並進行修改
 choice /C 12q /N /M "1=Hibernate[%Hibernate%]%file_size% // 2=Hybrid_Sleep[%Hybrid_Sleep%] // Q=Exit: "
